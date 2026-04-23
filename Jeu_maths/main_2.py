39c5ae9679f4337fa0ea7e5a172f2c359980d1d2
ayemoud <ehoumand1@gmail.com>
2026-04-23 15:27:44 +0000

    ajout boucle for pour les questions

--- Jeu_maths/main_2.py
+++ Jeu_maths/main_2.py
@@ -2,6 +2,8 @@ import random
 
 nb_min = 1
 nb_max = 10
+nb_questions = 4
+
 
 def poser_question():
     a = random.randint(nb_min, nb_max)
@@ -11,11 +13,12 @@ def poser_question():
     return reponse_int,a + b
 
 
-while True:
+for i in range(0, nb_questions):
+    print(f"Question n°{i + 1} sur 4")
     reponse_int, reponse_correcte = poser_question()
 
     if reponse_int == reponse_correcte :
-        print("Bonne reposne")
+        print("Bonne reponSe")
         break
     else:
         print("Mauvaise reponse")
